8db2919317504990163e534e5db3cf99eba30c40
Erik Goodman <erkorivo@catworks.arizona.edu>
2015-05-18 13:15:18 -0700

    Person uses files to load andd save data

--- app/src/main/java/com/example/glockenspiel/Person.java
+++ app/src/main/java/com/example/glockenspiel/Person.java
@@ -7,6 +7,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -67,27 +68,43 @@ public class Person {
         return this; // if user not found, use new user
     }
 
-    protected void save_person() throws FileNotFoundException {
-        FileInputStream fIn = openFileOutput("persons.txt");
+    protected void save_person(Context context) throws IOException {
+        // to change file in-place, we are reading whole file, making changes, then writing whole file.
+        File old = new File("persons.txt");
+        FileInputStream fIn = context.openFileInput("persons.txt");
         InputStreamReader isr = new InputStreamReader(fIn);
         BufferedReader bir = new BufferedReader(isr);
-        FileWriter fr = new FileWriter(fIn);
-        BufferedWriter br  = new BufferedWriter(fr);
 
+        File temp = File.createTempFile("tempfile", ".tmp");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(temp));
 
         String line = null;
         try {
             while ( (line=bir.readLine()) != null) {
                 if (line.compareTo(this.name)==0){ // we found a user!
-
-
-                    fIn.close();
-                    isr.close();
-                    bir.close();
+                    bw.write(this.name);
+                    this.score = new Integer(bir.readLine());
+                    bw.write(this.score);
+                    this.level = bir.readLine();
+                    bw.write(this.level);
+                    this.pattern = new Integer(bir.readLine());
+                    bw.write(this.pattern);
+                } else {
+                    bw.write(line);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        fIn.close();
+        isr.close();
+        bir.close();
+        bw.close();
+
+        // delete old file
+        old.delete();
+        //rename temp file to old filename
+        boolean success = temp.renameTo(old);
     }
 }
